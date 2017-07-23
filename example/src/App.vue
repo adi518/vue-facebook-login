2fb7f67fc21f45cfa11f074a9b1bf82c3ccab573
liran <liranc@wobi.co.il>
2017-07-23 19:37:53 +0300

    fixed example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-     <facebook-login appId="326022817735322">
+    <facebook-login appId="326022817735322">
     </facebook-login>
   </div>
 </template>
 
 <script>
-
+// FB.api(path, method, params, callback)
+FB.api('/me', 'get', function (response) {
+  console.log(response);
+})
 export default {
   name: 'app-app'
 }
