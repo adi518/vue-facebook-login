945e0d7ffbf78cc662e7066bac535b98a9d04d4c
Adi Sahar <adisahar1@hotmail.com>
2018-07-28 16:52:06 +0300

    Fix gh-pages dist path

--- docs/gh-pages/index.js
+++ docs/gh-pages/index.js
@@ -1,3 +1,4 @@
+var path = require('path')
 var ghpages = require('gh-pages')
 
-ghpages.publish('../dist', function (err) { err && console.error(err) })
+ghpages.publish(path.join(__dirname, '..', 'dist'), function (err) { err && console.error(err) })
