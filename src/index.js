5ef374e01fe28c6d83c2f02bc0311f40ee16cf42
Adi Sahar <adisahar1@hotmail.com>
2018-07-29 23:48:47 +0300

    Update export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import FBLogin from '@/components/FBLogin.vue'
-import FBLoginBare from '@/components/FBLogin.Bare.js'
 
 export { default as FBLogin } from '@/components/FBLogin.vue'
 export { default as FBLoginBare } from '@/components/FBLogin.Bare.js'
@@ -7,7 +6,6 @@ export { default as FBLoginBare } from '@/components/FBLogin.Bare.js'
 export const Install = {
   install(Vue, config) {
     Vue.component(FBLogin.name, FBLogin)
-    Vue.component(FBLoginBare.name, FBLoginBare)
   }
 }
 
