d1c4d2eb71c5e35cbb8e5777c2081dce69139bb3
Adi Sahar <adisahar1@hotmail.com>
2018-07-30 22:23:01 +0300

    Fix route name

--- docs/src/router.js
+++ docs/src/router.js
@@ -12,7 +12,7 @@ export const routes = [
   },
   {
     path: '/privacy-policy',
-    name: 'Privacy-Policy',
+    name: 'Privacy Policy',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
