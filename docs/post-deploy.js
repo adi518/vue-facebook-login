02f4cd50c09c25b14a8640836945ba57a853d922
Adi Sahar <adisahar1@hotmail.com>
2018-09-06 15:08:30 +0300

    Update post-deploy

--- docs/post-deploy.js
+++ docs/post-deploy.js
@@ -1,4 +1,4 @@
 const pkg = require('./package.json')
 const { exec } = require('child_process')
 
-exec(`git add package.json package-lock.json && git commit -m "Docs ${pkg.version}"`)
+exec(`git add package.json package-lock.json && git commit -m "Docs ${pkg.version}" && git push`)
