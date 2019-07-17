130dfbbaab1dd93a6037ac0b167c0eea3687668b
Adi Sahar <adisahar1@hotmail.com>
2019-07-17 21:25:29 +0300

    fix docs vulnerabilities

--- docs/vue.config.js
+++ docs/vue.config.js
@@ -1,13 +1,9 @@
 const isProduction = process.env.NODE_ENV === 'production'
 const config = {}
 
-if (isProduction) {
-  config.baseUrl = './'
-}
-
 config.chainWebpack = config => {
   if (isProduction) {
-    config.output.publicPath('./')
+    config.publicPath = './'
   }
   config.module
     .rule('markdown')
