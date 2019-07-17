359b4cd7c2aff7e87bc9aa6866b0de7e0cb095b2
Adi Sahar <adisahar1@hotmail.com>
2019-07-17 23:06:17 +0300

    Docs 1.0.26

--- docs/vue.config.js
+++ docs/vue.config.js
@@ -1,10 +1,11 @@
 const isProduction = process.env.NODE_ENV === 'production'
 const config = {}
 
+if (isProduction) {
+  config.publicPath = './'
+}
+
 config.chainWebpack = config => {
-  if (isProduction) {
-    config.output.publicPath = './'
-  }
   config.module
     .rule('markdown')
     .test(/\.md$/)
